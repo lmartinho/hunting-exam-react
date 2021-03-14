e4fb3be061d1f81f8706b392cab8c36a21d279ec
Luís Martinho <lmartinho@gmail.com>
2021-03-14 09:09:54 +0000

    Re-indent service worker code

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,20 +1,19 @@
 self.addEventListener('install', (event) => {
-    console.log('installing');
-    event.waitUntil(
-      caches.open('v1').then((cache) => {
-        return cache.addAll([
-          './',
-          './static/js/bundle.js',
-          './static/js/vendors~main.chunk.js',
-          './static/js/main.chunk.js'
-        ]);
-      })
-    );
-  });
+  console.log('installing');
+  event.waitUntil(
+    caches.open('v1').then((cache) => {
+      return cache.addAll([
+        './',
+        './static/js/bundle.js',
+        './static/js/vendors~main.chunk.js',
+        './static/js/main.chunk.js'
+      ]);
+    })
+  );
+});
 
-
-  self.addEventListener('fetch', (event) => {
-    event.respondWith(
-      caches.match(event.request)
-    );
-  });
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+  );
+});
